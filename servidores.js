f482621866edb48aee2220cdfdfac02853151a9b
root <mgiraudo@ies21.edu.ar>
2023-10-31 10:59:09 -0300

    .

--- servidores.js
+++ servidores.js
@@ -1,6 +1,14 @@
 const listado = {
-    nombre: "serv",
+    nombre: "Lista de servidores",
+    
     servidores:[
+        {
+            nombre: "rustico",
+            jugadores: "100/200",
+            direccionip: "direccion ip 123.23.2.123",
+            link: "https://" //absoluta si es a otra pag.
+            //progress bar?
+        },
         {
             nombre: "rustico",
             jugadores: "100/200",
