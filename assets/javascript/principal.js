ce4c50837500d3933aaced197cb25eaa4e1df262
gaspar verdu <gasparverdu07@gmail.com>
2023-11-06 12:29:41 -0300

    js

--- assets/javascript/principal.js
+++ assets/javascript/principal.js
@@ -1,2 +1,17 @@
-import listado from "./servidores.js" 
-console.log(listado)
+//import listado from "./servidores.js" 
+//console.log(listado)
+let servidores;
+
+fetch("../../servidores.json")
+    .then((datos)=>{
+        //si lo paso de un then a otro tengo que hacer un return
+        return datos.json()
+    }
+)
+.then(
+    (datos)=>{
+
+        servidores = datos
+        console.log(servidores)
+    }
+)
--- assets/javascript/servidores.js
+++ assets/javascript/servidores.js
@@ -15,8 +15,36 @@ const listado = {
             direccionip: "direccion ip",
             link: "https://" //absoluta si es a otra pag.
             //progress bar?
-        }
+        },
+        {
+            nombre: "rustico",
+            jugadores: "100/200",
+            direccionip: "direccion ip 123.23.2.123",
+            link: "https://" //absoluta si es a otra pag.
+            //progress bar?
+        },
+        {
+            nombre: "rustico",
+            jugadores: "100/200",
+            direccionip: "direccion ip 123.23.2.123",
+            link: "https://" //absoluta si es a otra pag.
+            //progress bar?
+        },
+        {
+            nombre: "rustico",
+            jugadores: "100/200",
+            direccionip: "direccion ip 123.23.2.123",
+            link: "https://" //absoluta si es a otra pag.
+            //progress bar?
+        },
+        {
+            nombre: "rustico",
+            jugadores: "100/200",
+            direccionip: "direccion ip 123.23.2.123",
+            link: "https://" //absoluta si es a otra pag.
+            //progress bar?
+        },
     ]
 }
 
-export default listado
+//export default listado
